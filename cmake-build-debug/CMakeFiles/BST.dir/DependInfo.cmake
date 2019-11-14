9aa417eb7cfa34f086b520e9a7170803d3885db1
Sachin Desai <sachin1400d@gmail.com>
2019-11-14 12:59:16 -0600

    Initial Commit

---
+++ cmake-build-debug/CMakeFiles/BST.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/sachin.desai/Documents/GitHub/BST/BST312.cpp" "/Users/sachin.desai/Documents/GitHub/BST/cmake-build-debug/CMakeFiles/BST.dir/BST312.cpp.o"
+  "/Users/sachin.desai/Documents/GitHub/BST/main.cpp" "/Users/sachin.desai/Documents/GitHub/BST/cmake-build-debug/CMakeFiles/BST.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
